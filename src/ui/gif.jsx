a45668a0d05fe5d7efa463b594f01fbe2e30bd64
Abdoullah Sghaier <sghaierabdoullah@gmail.com>
2024-04-16 10:10:57 +0100

    n

--- src/ui/gif.jsx
+++ src/ui/gif.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 export default function Gif(props) {
-  return (<Image src={props.src} alt={props.alt} width={120} height={120} />
+  return (<Image src={props.src} alt={props.alt} width={150} height={150} />
     
   )
 }
