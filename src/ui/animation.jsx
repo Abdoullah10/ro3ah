249c8dab6466d62180d876f1a17f98ff8523c46c
abdoullah <sghaierabdoullah@gmail.com>
2024-04-18 17:31:04 +0100

    z-index

--- src/ui/animation.jsx
+++ src/ui/animation.jsx
@@ -1,22 +1,61 @@
- "use client"
+"use client"
 import { useEffect, useRef, useState } from 'react';
 import { Player } from '@lordicon/react';
+const icons = {
+    superstar: {
+        src: require('./wired-outline-2239-star-of-bethlehem.json'),
+        size: 120,
+        states: {
+            reveal: 'in-reveal', // verify state name in the official documentation
+            hover: 'hover-detach',
+            click: 'morph-detach',
+        },
+    },
+    puzzle: {
+        src: require('./wired-outline-186-puzzle.json'),
+        size: 120,
+        states: {
+            reveal: 'in-reveal', // verify state name in the official documentation
+            hover: 'hover-detach',
+            click: 'morph-detach',
+        },
+    },
+    stars:{
+      src: require('./wired-outline-1865-shooting-stars.json'),
+      size: 120,
+      states: {
+          reveal: 'in-reveal', // verify state name in the official documentation
+          hover: 'hover-detach',
+          click: 'morph-detach',
+      },
 
-export default function Lord(props) {    
+    }
+    }
+
+export default function Lord(props) {   
+    const [icon, setIcon] = useState(icons[props.icon]);
     const playerRef = useRef(null);
-    const [state, setState] = useState('in-reveal');
+    const [state, setState] = useState();
+    const onHover = () => {
+        playerRef.current?.goToFirstFrame()
+        setState(icon.states.hover)
+        playerRef.current?.play()
+    }
+    
     useEffect(() => {
         playerRef.current?.playFromBeginning();
     }, [])
     return (
       
+        <div onMouseOver={onHover} >
         <Player 
           ref={playerRef} 
-          size={96} 
-          icon={require(props.src)}
+          size={icon.size} 
+          icon={icon.src}
           state={state}
-
-        />
+       
+       />
+       </div>
        
     );
     
---
+++ src/ui/icon.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default (function Icon() {
+  return (
+      <Image
+        src="/left-arrow.svg"
+        alt="arrow-left "
+        width={10}
+        height={10}
+        quality={100}
+        //className="blur-sm"
+      />
+      );})
