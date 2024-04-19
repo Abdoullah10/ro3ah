e4af7cd4d7393b0211b2eb11c5ed385415096007
abdoullah <sghaierabdoullah@gmail.com>
2024-04-19 08:37:27 +0100

    delete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 export const metadata = {
-  title: "ABOU-Game",
-  description: "game of guess",
+  title: "رعاة",
+  description: "منصة رعاة",
 };
 
 export default function RootLayout({ children }) {
